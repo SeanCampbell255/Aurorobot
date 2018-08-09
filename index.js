ebfef70e1d1ea8d8f7a9ba9b49bfac43c258913a
Sean Campbell <campbellsean211@gmail.com>
2018-08-09 15:57:04 -0400

    added config, ping pong
    config for token and prefix, ping pong event

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+//requires discord.js
 const Discord = require('discord.js');
+
+//require config file
+const {prefix, token} = require('./config.json');
+//starts new Discord client
 const client = new Discord.Client();
 
+//logs to console when the bot is up
 client.on('ready', () => {
     console.log('Ready!');
+});
+
+//reads all messages
+client.on('message', message => {
+  //!ping gets response 'Pong.'
+    if (message.content === `${prefix}ping`){
+      message.channel.send('Pong. ' + message.guild.name);
+    }
 });
 
 //NEVER COMMIT WITH THE TOKEN IN HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -11,7 +25,8 @@ client.on('ready', () => {
 //NEVER COMMIT WITH THE TOKEN IN HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //NEVER COMMIT WITH THE TOKEN IN HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //NEVER COMMIT WITH THE TOKEN IN HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-client.login('Token');
+//gets the token from config, logs bot into Discord
+client.login(token);
 //NEVER COMMIT WITH THE TOKEN IN HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //NEVER COMMIT WITH THE TOKEN IN HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //NEVER COMMIT WITH THE TOKEN IN HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
